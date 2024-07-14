8cb121e095141d8f74fda65c626e2f390b093bc7
Alex Fonseca <sousaalex1605@gmail.com>
2024-07-15 00:24:04 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 import asyncio
 import websockets
 
-async def get_server_response():
-  async with websockets.connect("wss://serverproducts-9lt2.onrender.com", port=8080) as websocket:
-    await websocket.send("oi")
-    response = await websocket.recv()
-    return response
+async def send_message():
+    uri = "wss://serverproducts-9lt2.onrender.com"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("oi")
+        print(f"> Enviado: oi")
+        
+        response = await websocket.recv()
+        print(f"< Recebido: {response}")
 
-async def main():
-  server_response = await get_server_response()
-  print(f"Resposta do servidor: {server_response}")
-
-asyncio.run(main())
+# Executa o script
+asyncio.get_event_loop().run_until_complete(send_message())
