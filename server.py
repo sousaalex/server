2db6c0a7593f8d1816a2b317c0005b8feae8e414
Alex Fonseca <sousaalex1605@gmail.com>
2024-07-14 15:27:36 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import websockets
 
@@ -7,7 +8,10 @@ async def echo(websocket, path):
         await websocket.send(message)
         print(f"Mensagem enviada de volta: {message}")
 
-start_server = websockets.serve(echo, "localhost", 8765)
+# Get port number from environment variable (default 8765)
+ws_port = int(os.environ.get("WS_PORT", 8765))
+
+start_server = websockets.serve(echo, "0.0.0.0", ws_port)  # Listen on all interfaces
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
