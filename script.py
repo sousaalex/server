c1e041f2f826b976e40e9e193b872767f3525fd3
Alex Fonseca <sousaalex1605@gmail.com>
2024-07-15 00:52:58 +0100

    testando o componente script.py

--- script.py
+++ script.py
@@ -1,28 +1,43 @@
 import asyncio
 import websockets
+from aiohttp import web
 
 async def send_message():
     uri = "wss://serverproducts-9lt2.onrender.com"
     
-    # Tenta conectar várias vezes
     for attempt in range(5):
         try:
             async with websockets.connect(uri, timeout=10) as websocket:
-                await websocket.send("oiAlex")
-                print(f"> Enviado: oi")
+                await websocket.send("Hello World")
+                print(f"> Enviado: hello world")
 
                 response = await websocket.recv()
                 print(f"< Recebido: {response}")
-                return  # Conexão bem-sucedida, sair da função
+                return response  # Retorna a resposta recebida do WebSocket
 
         except (websockets.exceptions.InvalidURI, websockets.exceptions.InvalidHandshake, websockets.exceptions.ConnectionClosedError) as e:
             print(f"Falha na tentativa {attempt + 1}: {e}")
-            await asyncio.sleep(5)  # Espera 5 segundos antes de tentar novamente
+            await asyncio.sleep(5)
         except asyncio.TimeoutError:
             print(f"Tentativa {attempt + 1} de conexão expirou.")
             await asyncio.sleep(5)
 
     print("Não foi possível conectar ao servidor WebSocket após várias tentativas.")
+    return None  # Retorna None se não conseguir conectar
 
-# Executa o script
-asyncio.run(send_message())
+async def handle(request):
+    response = await send_message()  # Chama a função para enviar a mensagem via WebSocket
+    if response:
+        return web.Response(text=f"Resposta do WebSocket: {response}")
+    else:
+        return web.Response(text="Falha ao conectar ao servidor WebSocket.")
+
+async def init_app():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    return app
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(init_app())
+    web.run_app(app, port=8000)
